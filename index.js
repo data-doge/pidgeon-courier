75b385d5f14c685ac9cde8beb66b175aa8182bde
data-doge <eugene@enspiral.com>
2017-01-15 17:49:53 -0500

    pidgeon can move

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Game, AUTO } = window.Phaser
+const { Game, AUTO, Physics } = window.Phaser
+const { ARCADE } =  Physics
 const range = require('lodash.range')
 
 const game = new Game(800, 600, AUTO, '', {
@@ -14,8 +15,28 @@ function preload () {
 function create () {
   this.pidgeon = game.add.sprite(50, 50, 'pidgeon')
   this.pidgeon.animations.add('flying', range(12, 15), 10, true)
+
+  game.physics.startSystem(ARCADE)
+  game.physics.arcade.enable(this.pidgeon)
+
+  this.cursors = game.input.keyboard.createCursorKeys()
 }
 
 function update () {
   this.pidgeon.animations.play('flying')
+
+  this.pidgeon.body.velocity.x = 0
+  this.pidgeon.body.velocity.y = 0
+
+  const { left, right, up, down } = this.cursors
+
+  if (left.isDown) {
+    this.pidgeon.body.velocity.x = -100
+  } else if (right.isDown) {
+    this.pidgeon.body.velocity.x = 100
+  } else if (up.isDown) {
+    this.pidgeon.body.velocity.y = -100
+  } else if (down.isDown) {
+    this.pidgeon.body.velocity.y = 100
+  }
 }
--- notes.md
+++ notes.md
@@ -1,10 +1,8 @@
 ## todo
 
-- pidgeon exists
 - camera on pidgeon
 - start screen and end screen
 - long horizontal world
-- pidgeon can move left right up and down
 - gravity exists
 - you can get hurt by flying into things
   - what is hurt: blink screen
