4453abf1a4cd7e1c8383a59850d3d85fda684ee3
data-doge <eugene@enspiral.com>
2017-01-15 18:53:19 -0500

    there are ufos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Game, AUTO, Physics, Camera } = window.Phaser
-const { ARCADE } =  Physics
+const { ARCADE } = Physics
 const range = require('lodash.range')
+const random = require('lodash.random')
 
 const speed = 400
 const height = 407
@@ -17,6 +18,7 @@ console.log({ game })
 function preload () {
   game.load.image('city', './assets/city.jpg')
   game.load.spritesheet('pidgeon', './assets/master-pidgeon-spritesheet.png', 64, 64)
+  game.load.spritesheet('ufo', './assets/ufo.png', 58, 210)
 }
 
 function create () {
@@ -24,6 +26,16 @@ function create () {
 
   this.pidgeon = game.add.sprite(50, 50, 'pidgeon')
   this.pidgeon.animations.add('flying', range(12, 15), 10, true)
+
+  // adding ufos
+  this.ufos = game.add.group()
+  let x = random(200, 1000)
+  while (x < 4000) {
+    let y = random(25, 200)
+    this.ufos.create(x, y, 'ufo')
+    x += random(200, 1000)
+  }
+  this.ufos.callAll('animations.add', 'animations', 'abducting', range(0, 11), 10, true)
 
   game.physics.startSystem(ARCADE)
   game.physics.arcade.enable(this.pidgeon)
@@ -38,6 +50,7 @@ function create () {
 
 function update () {
   this.pidgeon.animations.play('flying')
+  this.ufos.callAll('animations.play', 'animations', 'abducting')
 
   this.pidgeon.body.velocity.x = 0
   this.pidgeon.body.velocity.y = 0
--- notes.md
+++ notes.md
@@ -1,10 +1,8 @@
 ## todo
 
-- camera on pidgeon
-- long horizontal world
-- start screen and end screen
 - you can get hurt by flying into things
   - what is hurt: blink screen
+- start screen and end screen
 
 ## stretch
 
