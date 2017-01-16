c0ee84bdc37e0e5d08e5a8177d690324db7543cf
data-doge <eugene@enspiral.com>
2017-01-15 20:47:37 -0500

    COOING

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Game, AUTO, Physics, Camera } = window.Phaser
+const { Game, AUTO, Physics, Camera, Keyboard } = window.Phaser
 const { ARCADE } = Physics
 const range = require('lodash.range')
 const random = require('lodash.random')
@@ -13,22 +13,27 @@ const game = new Game(800, 407, AUTO, '', {
   update
 })
 
-console.log({ game })
-
 function preload () {
+  game.load.audio('coo', './assets/coo.wav')
   game.load.image('city', './assets/city.jpg')
   game.load.spritesheet('pidgeon', './assets/master-pidgeon-spritesheet.png', 64, 64)
   game.load.spritesheet('ufo', './assets/ufo.png', 58, 210)
+  game.load.spritesheet('explosion', './assets/explosion.png', 64, 64)
 }
 
 function create () {
+  game.physics.startSystem(ARCADE)
+
   this.city = game.add.tileSprite(0, 0, worldWidth, height, 'city')
 
   this.pidgeon = game.add.sprite(50, 50, 'pidgeon')
   this.pidgeon.animations.add('flying', range(12, 15), 10, true)
 
+  this.explosion = game.make.sprite(-50, -50, 'explosion')
+  this.explosion.animations.add('exploding', range(0, 24), 10, false)
+
   // adding ufos
-  this.ufos = game.add.group()
+  this.ufos = game.add.physicsGroup()
   let x = random(200, 1000)
   while (x < 4000) {
     let y = random(25, 200)
@@ -36,12 +41,15 @@ function create () {
     x += random(200, 1000)
   }
   this.ufos.callAll('animations.add', 'animations', 'abducting', range(0, 11), 10, true)
+  this.ufos.enableBody = true
 
-  game.physics.startSystem(ARCADE)
+  this.coo = game.add.audio('coo')
+
   game.physics.arcade.enable(this.pidgeon)
   // this.pidgeon.body.gravity.y = 2000
 
   this.cursors = game.input.keyboard.createCursorKeys()
+  this.spacebar = game.input.keyboard.addKey(Keyboard.SPACEBAR)
 
   game.camera.follow(this.pidgeon, Camera.FOLLOW_LOCKON)
 
@@ -55,15 +63,26 @@ function update () {
   this.pidgeon.body.velocity.x = 0
   this.pidgeon.body.velocity.y = 0
 
-  const { left, right, up, down } = this.cursors
-
-  if (left.isDown) {
-    this.pidgeon.body.velocity.x = -speed
-  } else if (right.isDown) {
-    this.pidgeon.body.velocity.x = speed
-  } else if (up.isDown) {
-    this.pidgeon.body.velocity.y = -speed
-  } else if (down.isDown) {
-    this.pidgeon.body.velocity.y = speed
+  if (this.spacebar.isDown) {
+    this.coo.play()
   }
+
+  const { left, right, up, down } = this.cursors
+  if (left.isDown) { this.pidgeon.body.velocity.x = -speed }
+  if (right.isDown) { this.pidgeon.body.velocity.x = speed }
+  if (up.isDown) { this.pidgeon.body.velocity.y = -speed }
+  if (down.isDown) { this.pidgeon.body.velocity.y = speed }
+
+  game.physics.arcade.collide(this.pidgeon, this.ufos, (pidgeon, ufo) => {
+    const { x, y } = pidgeon.body
+    game.add.existing(this.explosion)
+    this.explosion.x = x
+    this.explosion.y = y
+    this.explosion.play('exploding')
+    this.explosion.animations.currentAnim.killOnComplete = true
+    this.coo.play()
+    pidgeon.kill()
+    ufo.kill()
+    this.ufos.remove(ufo)
+  })
 }
