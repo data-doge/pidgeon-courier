3b7590dc7ccda4437cba04579ff6a5ee20afec81
data-doge <eugene@enspiral.com>
2017-01-22 17:42:17 -0500

    asjldfad

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ menu.prototype = {
   preload: function () {
     this.load.audio('coo', './assets/coo.wav')
     this.load.image('city', './assets/city.jpg')
-    this.load.spritesheet('pidgeon', './assets/pidgeon-carrying-shit.png', 64, 64)
+    this.load.spritesheet('pidgeon', './assets/master-pidgeon-spritesheet.png', 64, 64)
     this.load.spritesheet('ufo', './assets/ufo.png', 58, 210)
     this.load.spritesheet('explosion', './assets/explosion.png', 64, 64)
   },
